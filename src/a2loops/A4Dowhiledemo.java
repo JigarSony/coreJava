9be73a8f6b0962e7ea1d477babe97633005d4b2f
Jigar Sony <sonijigar94@gmail.com>
2022-04-26 21:08:22 +0530

    do whileLoop

--- src/a2loops/A4Dowhiledemo.java
+++ src/a2loops/A4Dowhiledemo.java
@@ -1,20 +1,31 @@
 package a2loops;
+
 //Video-14
 public class A4Dowhiledemo {
 
 	public static void main(String[] args) {
-		
-		int j=0;
+		/*
+		 * In do while first it's executed 1 time and then it will check boolean
+		 * condition
+		 * 
+		 * 1 loop of execution is guaranteed in do while loop
+		 */
+
+		int j = 0;
 		do {
 			System.out.println(j);
 			j++;
-		}while(j<10);
+		} while (j < 10);
+		/*
+		 * 0 1 2 3 4 5 6 7 8 9
+		 */
+
+		int k = 20;
+		do {
+			System.out.println(k);
+			k++;
+		} while (k < 10);
 	}
-	
-	/*
-	 * In do while first it's executed 1 time and then
-	 * it will check boolean condition
-	 * 
-	 * one loop of execution is guaranteed in do while loop 
-	 */
+	// 20
+
 }
