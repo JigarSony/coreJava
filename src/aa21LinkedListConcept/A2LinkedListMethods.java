50e56dd6fee7b85b1b331fa8ed110c68c710c9f3
Jigar Sony <sonijigar94@gmail.com>
2022-08-31 17:25:31 +0530

    completed LL Methods - Video-19

--- src/aa21LinkedListConcept/A2LinkedListMethods.java
+++ src/aa21LinkedListConcept/A2LinkedListMethods.java
@@ -1,73 +1,3 @@
-# LinkedList
-
-![alt text](https://github.com/JigarSony/coreJava/blob/master/Screenshots/91.png?raw=true)
-
-![alt text](https://github.com/JigarSony/coreJava/blob/master/Screenshots/92.png?raw=true)
-
-![alt text](https://github.com/JigarSony/coreJava/blob/master/Screenshots/93.png?raw=true)
-
-![alt text](https://github.com/JigarSony/coreJava/blob/master/Screenshots/94.png?raw=true)
-
-![alt text](https://github.com/JigarSony/coreJava/blob/master/Screenshots/95.png?raw=true)
-
-![alt text](https://github.com/JigarSony/coreJava/blob/master/Screenshots/96.png?raw=true)
-
-## A1MyLinkedList
-
-```
-package aa21LinkedListConcept;
-
-public class A1MyLinkedList {
-	
-	Node head;
-	//creating own linkedList
-	class Node {
-		
-		int data;
-		Node next;
-		
-		Node(int data){
-			this.data = data;
-			next = null;
-		}
-	}
-	
-	//Printing Linked list
-	public void printLinedList() {
-		Node n = head;
-		
-		while(n != null) {
-			System.out.println(n.data);
-			n = n.next;
-		}
-	}
-
-	public static void main(String[] args) {
-		
-		A1MyLinkedList ll = new A1MyLinkedList();
-		
-		Node first = ll.new Node(10);
-		ll.head = first;
-		
-		Node second = ll.new Node(20);
-		first.next = second;
-		
-		Node third = ll.new Node (30);
-		second.next = third;
-		
-		ll.printLinedList();
-		/*
-		 * 10
-		 * 20
-		 * 30
-		 */
-	}
-}
-```
-
-## A2LinkedListMethods
-
-```
 package aa21LinkedListConcept;
 
 import java.util.Collections;
@@ -188,6 +118,7 @@ public class A2LinkedListMethods {
 		System.out.println(names);
 		//[]
 		
+		
 		//**** Another Linked List
 		LinkedList<String> lang = new LinkedList<String>();
 		
@@ -217,14 +148,12 @@ public class A2LinkedListMethods {
 		}
 		
 		//Collections
+		
 		//Sort
+		
 		Collections.sort(lang);
 		System.out.println(lang);
 		//[Java, JavaScript, Ruby, phython]
 	}
+
 }
-```
-
-![alt text](https://github.com/JigarSony/coreJava/blob/master/Screenshots/97.png?raw=true)
-
-![alt text](https://github.com/JigarSony/coreJava/blob/master/Screenshots/98.png?raw=true)
